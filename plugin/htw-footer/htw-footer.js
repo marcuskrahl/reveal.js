6138f84efdb1086bb399ddf12980bcfd0a5b58ee
Marcus Krahl <mail@marcuskrahl.de>
2016-03-31 11:15:28 +0200

    added plugin to create a correct footer for HTW presentations

---
+++ plugin/htw-footer/htw-footer.js
@@ -0,0 +1,48 @@
+(function () {
+    "use strict";
+
+    var slidePrefix = "Seite ";
+    var name = "Max Mustermann";
+    var title = "Eine Demopräsentation";
+    var date = (typeof fixedDate === "object" && fixedDate instanceof Date) ? fixedDate : new Date();
+    var dateString = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    dateString += ".";
+    dateString += date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
+    dateString += ".";
+    dateString += date.getFullYear();
+
+
+    function createFooter(slideNumber) {
+        var footerElement = document.createElement("footer");
+
+        var nameElement = document.createElement("div");
+        nameElement.textContent = name;
+        nameElement.classList.add("footer-name");
+        footerElement.appendChild(nameElement);
+
+        var titleElement = document.createElement("div");
+        titleElement.textContent = title;
+        titleElement.classList.add("footer-title");
+        footerElement.appendChild(titleElement);
+
+        var slideNumberElement = document.createElement("div");
+        slideNumberElement.textContent  = slidePrefix + slideNumber;
+        slideNumberElement.classList.add("footer-slide-number");
+        footerElement.appendChild(slideNumberElement);
+
+        var dateElement = document.createElement("div");
+        dateElement.textContent = dateString;
+        dateElement.classList.add("footer-date");
+        footerElement.appendChild(dateElement);
+
+        return footerElement;
+    }
+
+    var slides = document.querySelectorAll('.slides > section');
+
+    for(var slideNumber = 0; slideNumber < slides.length; slideNumber++) {
+        var slide = slides[slideNumber];
+        slide.appendChild(createFooter(slideNumber+1)); 
+    }
+
+})()
